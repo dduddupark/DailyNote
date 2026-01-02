d28464645457f167717dba4a03040dd292875446
Blue <bluepark@yanadoocorp.com>
2026-01-02 10:17:43 +0900

    Initial Commit

---
+++ DailyNote/DailyNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNoteApp.swift
+//  DailyNote
+//
+//  Created by yanadoo on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyNoteTests/DailyNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyNoteTests.swift
+//  DailyNoteTests
+//
+//  Created by yanadoo on 1/2/26.
+//
+
+import Testing
+@testable import DailyNote
+
+struct DailyNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
